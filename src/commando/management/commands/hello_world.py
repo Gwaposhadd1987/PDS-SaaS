9ff3f998605ba9400738c8013f91b15960bf5f64
Shadd Watsone <watson.shadd@outlook.com>
2025-03-18 18:23:43 -0400

    added commando and static files functionality

---
+++ src/commando/management/commands/hello_world.py
@@ -0,0 +1,10 @@
+import commando.helpers
+
+from typing import Any
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    
+    def handle(self, *args: Any, **options: Any ) -> str | None:
+        print("Hello World")
